038d233f1c1b50bd8a0a839459da804e1b7b15d9
JoacoAco <128651614+JoacoAco@users.noreply.github.com>
2023-03-23 00:04:56 -0300

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,10 @@
+ 
+numero = 1
+if numero == 1:
+    print ("Bien ahí masterclass ;)")
+elif numero == 0:
+    print ("Casi casi, vos podes")
+elif numero == 2:
+    print ("Casi casi, vos podes")
+else:
+    print ("Te dije 1 pelotudo, estás lejísimos")
