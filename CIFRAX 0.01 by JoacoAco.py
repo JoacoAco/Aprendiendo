406e3506069779ff41ebff4daed51b71dc1ba98b
JoacoAco <128651614+JoacoAco@users.noreply.github.com>
2023-03-24 02:45:02 -0300

    CIFRAX 0.01

---
+++ CIFRAX 0.01 by JoacoAco.py
@@ -0,0 +1,16 @@
+print("CIFRAX 0.01")
+num = input("""
+            Ingresa el número a cifrar: """)
+passcode = num
+passcode_1 = passcode*2
+passcode_2 = int(passcode_1)
+passcode_2 *=3.14159
+ 
+if type(passcode_2) == int:
+    print(f"Su número en el sistema de cifrado CIFRAX 0.01 es {passcode_2} y es un número entero")
+elif passcode_2 >= 150:
+    print (f"Su número en el sistema de cifrado CIFRAX 0.01 es {passcode_2} y es un número recomendado")
+elif type(passcode_2) == float:   
+    print(f"Su número en el sistema de cifrado de Joaquín es {passcode_2} y es un número decimal no recomendado")
+
+
