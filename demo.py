a10657ffe8bf820ecdab913120488777b0015973
JoacoAco <128651614+JoacoAco@users.noreply.github.com>
2023-03-24 02:50:08 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
- 
-numero = 1
+ numero = input("Escrbí el 1 a continuación, es para una misión secreta importante: ")
 if numero == 1:
-    print ("Bien ahí masterclass ;)")
+    print ("Gracias masterclass ;)")
 elif numero == 0:
     print ("Casi casi, vos podes")
+elif type(numero) == str:
+    print("Por qué ingresaste letras salamín?")
 elif numero == 2:
     print ("Casi casi, vos podes")
 else:
