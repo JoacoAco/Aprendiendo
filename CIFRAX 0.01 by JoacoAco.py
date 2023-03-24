264451db2c835ae8941a91f6fd3f1afbd6a7f9b6
JoacoAco <128651614+JoacoAco@users.noreply.github.com>
2023-03-24 02:54:33 -0300

    Update CIFRAX 0.01 by JoacoAco.py

--- CIFRAX 0.01 by JoacoAco.py
+++ CIFRAX 0.01 by JoacoAco.py
@@ -5,12 +5,12 @@ passcode = num
 passcode_1 = passcode*2
 passcode_2 = int(passcode_1)
 passcode_2 *=3.14159
- 
+
 if type(passcode_2) == int:
     print(f"Su número en el sistema de cifrado CIFRAX 0.01 es {passcode_2} y es un número entero")
 elif passcode_2 >= 150:
     print (f"Su número en el sistema de cifrado CIFRAX 0.01 es {passcode_2} y es un número recomendado")
 elif type(passcode_2) == float:   
-    print(f"Su número en el sistema de cifrado de Joaquín es {passcode_2} y es un número decimal no recomendado")
+    print(f"Su número en el sistema de cifrado CIFRAX 0.01 es {passcode_2} y es un número decimal no recomendado")
 
 
